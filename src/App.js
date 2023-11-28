3f3b864833c1656305142637fd1df66242bdfa96
@hunghh <hunghh@unicloud.com.vn>
2023-11-28 17:15:14 +0700

    Add Rendering list && random images with API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
-import React from 'react';
-import Navbar from './components/Navbar';
+import React from "react";
+import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/App.css";
+import { YoutubeData } from "./YoutubeData";
 
 function App() {
   return (
-    <div className='App'>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact Component={Home } />
-        </Routes>
-      </Router>
+    <div>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" exact Component={Home} />
+          </Routes>
+        </Router>
+      </div>
+      {/* Rendering list */}
+      <div className="youtube-list">
+        {YoutubeData.map((item, index) => {
+          let image = "";
+          image = `${item.image}${item.id}`;
+          return (
+            <YoutubeItem
+              key={item.id}
+              image={image}
+              avatar={item.avatar}
+              title={item.title}
+              author={item.author}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+function YoutubeItem(props) {
+  return (
+    <div className="youtube-item">
+      <div className="youtube-image">
+        <img src={props.image} alt="" />
+      </div>
+
+      <div className="youtube-footer">
+        <img src={props.avatar} alt="" className="youtube-avatar" />
+
+        <div className="youtube-info">
+          <h3 className="youtube-title">{props.title || "SuperCar"}</h3>
+          <h4 className="youtube-author">{props.author || "Ha Huu Hung"}</h4>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/YoutubeData.js
@@ -0,0 +1,75 @@
+export const YoutubeData = [
+  {
+    id: 1,
+    image:
+      // "https://images.unsplash.com/photo-1580414057403-c5f451f30e1c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGFtYm9yZ2hpbml8ZW58MHx8MHx8fDA%3D",
+      "https://source.unsplash.com/random/500x250/?super-car", // => Copy dòng này dán vào item khác để thấy sự khác biệt
+    avatar:
+      "https://plus.unsplash.com/premium_photo-1668211358705-b758b5d7f7cf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjV8fGRvZ3xlbnwwfHwwfHx8MA%3D%3D",
+    author: "Dog",
+    title: "SuperCar Lamborghini Aventador",
+  },
+
+  {
+    id: 2,
+    image: "https://source.unsplash.com/random/500x250/?super-car",
+    avatar:
+      "https://images.unsplash.com/photo-1618826411640-d6df44dd3f7a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2F0fGVufDB8fDB8fHww",
+    author: "Cat",
+    title: "SuperCar BMW I8",
+  },
+
+  {
+    id: 3,
+    image: "https://source.unsplash.com/random/500x250/?super-car",
+    avatar:
+      "https://images.unsplash.com/photo-1474511320723-9a56873867b5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW5pbWFsfGVufDB8fDB8fHww",
+    author: "Fox",
+    title: "SuperCar Roll-Royce",
+  },
+
+  {
+    id: 4,
+    image: "https://source.unsplash.com/random/500x250/?super-car",
+    avatar:
+      "https://images.unsplash.com/photo-1555169062-013468b47731?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmlyZHxlbnwwfHwwfHx8MA%3D%3D",
+    author: "Bird",
+    title: "SuperCar Bugatti Chiron",
+  },
+
+  {
+    id: 5,
+    image: "https://source.unsplash.com/random/500x250/?super-car",
+    avatar:
+      "https://images.unsplash.com/photo-1484406566174-9da000fda645?w:500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YW5pbWFsfGVufDB8fDB8fHww",
+    author: "Dog",
+    title: "SupeCar Lexus",
+  },
+
+  {
+    id: 6,
+    image: "https://source.unsplash.com/random/500x250/?super-car",
+    avatar:
+      "https://images.unsplash.com/photo-1437622368342-7a3d73a34c8f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YW5pbWFsfGVufDB8fDB8fHww",
+    author: "Cat",
+    title: "SuperCar Pagani",
+  },
+
+  {
+    id: 7,
+    image: "https://source.unsplash.com/random/500x250/?super-car",
+    avatar:
+      "https://images.unsplash.com/photo-1602491453631-e2a5ad90a131?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YW5pbWFsfGVufDB8fDB8fHww",
+    author: "Fox",
+    title: "SuperCar Roll-Royce",
+  },
+
+  {
+    id: 8,
+    image: "https://source.unsplash.com/random/500x250/?super-car",
+    avatar:
+      "https://images.unsplash.com/photo-1522069169874-c58ec4b76be5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGFuaW1hbHxlbnwwfHwwfHx8MA%3D%3D",
+    author: "Bird",
+    title: "SuperCar Bugatti Chiron",
+  },
+];
